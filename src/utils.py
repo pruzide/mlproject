9487c2b5362986f98b340d2ed6e982014e9c26dc
pruzide <singhgourav0565@gmail.com>
2025-02-01 16:43:55 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.logger import logging
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,14 +22,22 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
-            model.fit(X_train, y_train)  # Train model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -39,7 +48,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             test_model_score = r2_score(y_test, y_test_pred)
 
              # Print the R² scores
-            print(f"{list(models.keys())[i]}: Train R² = {train_model_score:.4f}, Test R² = {test_model_score:.4f}")
+            print(f"{list(models.keys())[i]}: Train R² = {train_model_score:.4f}, Test R² = {test_model_score:.4f}")  ## .4f is to control the number of decimal places to show in the output to not overcrowd the terminal space 
 
             report[list(models.keys())[i]] = test_model_score
 
