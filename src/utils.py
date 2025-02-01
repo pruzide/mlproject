44e749327665130b9ea400a73cbed2ec295cefe0
pruzide <singhgourav0565@gmail.com>
2025-02-01 21:27:41 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -29,13 +29,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
-            para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]] ## list(model.keys())[i] will give name of algorithm and we have created param dictionary in such a way so that by accessing the name of the algorithm we can access the respective parameters to test out in gridsearchCV to find out the best parameters
 
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_) ## ** unpacks dictionary so that set_params() method received keyword arguments and not a dictionary 
+            model.fit(X_train,y_train)   ## gs.best_params_ gives a dictionary contating best params
 
             #model.fit(X_train, y_train)  # Train model
 
@@ -53,6 +53,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
